9f3b44076f9cc89e9d618c55f648d3469a185f85
= <920347125@qq.com>
2020-06-21 11:52:18 +0800

    suit for arm32app
    support arm 32bit apps with anbox

--- products/arm64/device.mk
+++ products/arm64/device.mk
@@ -15,11 +15,11 @@
 #
 
 # Copy the 64-bit primary, 32-bit secondary zygote startup script
-PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64.rc:root/init.zygote64.rc
+PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
 
 # Set the zygote property to select the 64-bit primary, 32-bit secondary script
 # This line must be parsed before the one in core_minimal.mk
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64_32
 
-TARGET_SUPPORTS_32_BIT_APPS := false
+TARGET_SUPPORTS_32_BIT_APPS := true
 TARGET_SUPPORTS_64_BIT_APPS := true
